a19d7de3e2c89c09d98bcaabe1b4aac94af74f71
kaylasam <kaylasamanthaa@gmail.com>
2021-11-03 01:29:43 -0500

    deleted dummy data generator from prev project

--- app/src/main/java/edu/uark/finalproject/data/dummydata/DummyDataGeneratorCallback.java
+++
@@ -1,9 +0,0 @@
-package edu.uark.finalproject.data.dummydata;
-
-import java.util.List;
-
-public interface DummyDataGeneratorCallback {
-
-    void dummyDataCreated(List<Message> messages);
-    void onDataNotCreated();
-}
