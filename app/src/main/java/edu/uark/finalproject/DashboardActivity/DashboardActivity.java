aee71d492378fe665c069e255773841306384070
kaylasam <kaylasamanthaa@gmail.com>
2021-11-03 17:22:23 -0500

    created navigation drawer activity that MAY rep menu, created dashboard activity

---
+++ app/src/main/java/edu/uark/finalproject/DashboardActivity/DashboardActivity.java
@@ -0,0 +1,15 @@
+package edu.uark.finalproject.DashboardActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import edu.uark.finalproject.R;
+
+import android.os.Bundle;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
