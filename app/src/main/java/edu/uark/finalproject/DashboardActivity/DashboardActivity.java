dcb4ad3ba49d92df5ee9671a04bbf77a83642716
Daniel Salazar <admin@Daniels-MacBook-Pro.local>
2021-11-05 05:19:35 -0500

    Added button listeners for main menu

--- app/src/main/java/edu/uark/finalproject/DashboardActivity/DashboardActivity.java
+++ app/src/main/java/edu/uark/finalproject/DashboardActivity/DashboardActivity.java
@@ -1,11 +1,19 @@
 package edu.uark.finalproject.DashboardActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
 import edu.uark.finalproject.R;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class DashboardActivity extends AppCompatActivity {
+public class DashboardActivity extends AppCompatActivity implements View.OnClickListener {
+    private String TAG = "DashboardActivity";
+    CardView btn_parent_profile, btn_child_profile, btn_vehicle_profile, btn_schedule_pickup,
+            btn_map_view, btn_review_pickups;
+    CardView cardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +21,42 @@ public class DashboardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        btn_parent_profile = (CardView)findViewById(R.id.ParentProfileCard);
+        btn_child_profile = (CardView)findViewById(R.id.ChildProfileCard);
+        btn_vehicle_profile = (CardView)findViewById(R.id.VehicleProfileCard);
+        btn_schedule_pickup = (CardView)findViewById(R.id.SchedulePickupCard);
+        btn_map_view = (CardView)findViewById(R.id.MapViewCard);
+        btn_review_pickups = (CardView)findViewById(R.id.ReviewPickupsCard);
+        btn_parent_profile.setOnClickListener(this);
+        btn_child_profile.setOnClickListener(this);
+        btn_vehicle_profile.setOnClickListener(this);
+        btn_schedule_pickup.setOnClickListener(this);
+        btn_map_view.setOnClickListener(this);
+        btn_review_pickups.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.ParentProfileCard:
+                Toast.makeText(this, "ParentProfileCard", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ChildProfileCard:
+                Toast.makeText(this, "ChildProfileCard", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.VehicleProfileCard:
+                Toast.makeText(this, "VehicleProfileCard", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.SchedulePickupCard:
+                Toast.makeText(this, "SchedulePickupCard", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.MapViewCard:
+                Toast.makeText(this, "MapViewCard", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ReviewPickupsCard:
+                Toast.makeText(this, "ReviewPickupsCard", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
